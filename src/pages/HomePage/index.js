0c4b8e059d99290bb835acad9e08aaf681f4fded
Mike <mike@mikevitelli.com>
2021-01-03 18:07:34 -0500

    margin change again

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,18 +1,66 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import AllSpeak from "../../components/Cards/AllSpeak";
+import ChopCheese from "../../components/Cards/ChopCheese";
+import RememberDat from "../../components/Cards/RememberDat";
 import Main from "../../components/Main";
-// import { Container, Row, Col } from "react-bootstrap";
-// import AllSpeak from "../../components/Cards/AllSpeak";
-// import ChopCheese from "../../components/Cards/ChopCheese";
-// import RememberDat from "../../components/Cards/RememberDat";
-import Projects from "../ProjectsPage";
 import "../../App.css";
-// import ReactPageScroller from "react-page-scroller";
 
 const HomePage = () => {
   return (
     <>
-      {/* <Main className="mygh repo clone KendraNeves/portfolio-react-3 align-items-center" /> */}
-      <Projects />
+      <Container>
+        <Row>
+          <div style={{ height: 100 }}></div>
+        </Row>
+        <Row className="align-items-center">
+          <Col>
+            <Main />
+          </Col>
+        </Row>
+        <Row>
+          <div style={{ height: 150 }}></div>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row className="align-items-center">
+          <div style={{ height: 150 }}></div>
+        </Row>
+        <Row>
+          <Col>
+            <AllSpeak />
+          </Col>
+        </Row>
+        <Row>
+          <div style={{ height: 150 }}></div>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          <div style={{ height: 150 }}></div>
+        </Row>
+        <ChopCheese />
+        <Row>
+          <div style={{ height: 150 }}></div>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          <div style={{ height: 150 }}></div>
+        </Row>
+        <Row>
+          <Col>
+            <RememberDat />
+          </Col>
+        </Row>
+        <Row>
+          <div style={{ height: 150 }}></div>
+        </Row>
+      </Container>
     </>
   );
 };
