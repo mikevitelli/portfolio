cf5e3f94079890e0ce133c746cd6f121881a5c44
Mike <mike@mikevitelli.com>
2021-01-03 21:30:56 -0500

    added project seed data

--- src/projectData.js
+++ src/projectData.js
@@ -21,8 +21,46 @@ export const projectData = [
     description:
       "allSpeak is a universal language chat room application. In these modern times, connecting with people from around the globe through the Internet has never been easier; however, there is still a significant language barrier involved. At allSpeak, we wanted to create a chatroom where people speaking multiple different languages can still connect and share experiances in the same conversation. Using allSpeak, any user can can enter a chatroom using only their own common tounge. Simply set you prefered language and allSpeak will automagically live translate all of your messages.",
     stack:
-      "MERN, MongoDB, Express, React.js, Node.js, Google maps Javascript and Geocode APIs, Passport authentication, Bootstrap", //
+      "MERN, MongoDB, Express, React.js, Node.js, Google maps Javascript and Geocode APIs, Passport authentication, Bootstrap",
     repo: "https://github.com/kylegold/all-speak",
+  },
+  {
+    title: "chopCheese",
+    img:
+      "https://github.com/mikevitelli/chopCheese/raw/main/assests/chopCheese-logo-vector-black-txt.png",
+    alt: "chopCheese Logo",
+    deployedUrl: "https://mikevitelli.github.io/chopCheese/",
+    description: `We call ourselves "The Corner Store of the Internet." Hate all the hastle of looking for good, quality memes? We do too. That's why we built this wonderful application. chopCheese is a content aggregation platform that does all the leg work of finding funny memes for you. Simply hop in the search bar and search your heart out. Scroll a bit down and copy whatever fun stuff you find to your clipboard and share with whomever your shall please. From your Grandma to your Girlfriend, everbody loves a chopCheese!`,
+    stack: "SCSS, Less, CSS, JavaScript, HTML",
+    repo: "https://github.com/mikevitelli/chopCheese",
+  },
+  {
+    title: "chopCheese",
+    img:
+      "https://github.com/mikevitelli/chopCheese/raw/main/assests/chopCheese-logo-vector-black-txt.png",
+    alt: "chopCheese Logo",
+    deployedUrl: "https://mikevitelli.github.io/chopCheese/",
+    description: `We call ourselves "The Corner Store of the Internet." Hate all the hastle of looking for good, quality memes? We do too. That's why we built this wonderful application. chopCheese is a content aggregation platform that does all the leg work of finding funny memes for you. Simply hop in the search bar and search your heart out. Scroll a bit down and copy whatever fun stuff you find to your clipboard and share with whomever your shall please. From your Grandma to your Girlfriend, everbody loves a chopCheese!`,
+    stack: "SCSS, Less, CSS, JavaScript, HTML",
+    repo: "https://github.com/mikevitelli/chopCheese",
+  },
+  {
+    title: "rememberDat",
+    img:
+      "https://github.com/mikevitelli/rememberDat/raw/main/public/assets/rememberDat_logo_startpage.png",
+    alt: "rememberDat Logo",
+    deployedUrl: "https://remember-dat.herokuapp.com/",
+    description: `A website that brings you back to the simpler times. RememberDat is a Nostalgia engine that generates sweet, sweet dopamine through memories and media. Users will get a “blast from the past” through music, news articles, photos/memes, games, and aesthetic UI. GIVEN that I want an Escape from these troubling times
+    and want to get a blast from the past
+    I WANT to see stuff that reminds me 90s Pop Culture
+    SO THAT I can remember how far Technology and UI has come over the last 30 years. 
+    WHEN I create my rememberDat account
+    THEN I am given a list of categories to choose from
+    WHEN I select my topic I am able to get my dopamine dose of the good ol' days.
+    THROUGH APIs and features that deliver curated 90s era content. `,
+    stack:
+      "This Project uses Express, Travis-CI, Handlebars, Sequelize, Eslint, and Passport.",
+    repo: "https://github.com/mikevitelli/rememberDat",
   },
 ];
 
