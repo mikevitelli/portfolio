d090c1e9445411ece00a019c7a95b5ae3eef8ab5
Mike <mike@mikevitelli.com>
2021-01-03 21:04:34 -0500

    added project data file with seed

---
+++ src/projectData.js
@@ -0,0 +1,29 @@
+// const ProjectCard = (props) => {
+//   const {
+//     projectPic,
+//     alt,
+//     cardTitle,
+//     description,
+//     techStack,
+//     githubUrl,
+//     deployedUrl,
+//     datePosted,
+//   } = props;
+
+export const projectData = [
+  {
+    title: "allSpeak",
+    img:
+      "https://github.com/kylegold/all-speak/raw/main/client/src/assets/logo/png/all_speak_v2_Logo_White.png",
+    alt: "allSpeak Logo",
+    deployedUrl:
+      "https://github.com/kylegold/all-speak/deployments/activity_log?environment=immense-journey-22247",
+    description:
+      "allSpeak is a universal language chat room application. In these modern times, connecting with people from around the globe through the Internet has never been easier; however, there is still a significant language barrier involved. At allSpeak, we wanted to create a chatroom where people speaking multiple different languages can still connect and share experiances in the same conversation. Using allSpeak, any user can can enter a chatroom using only their own common tounge. Simply set you prefered language and allSpeak will automagically live translate all of your messages.",
+    stack:
+      "MERN, MongoDB, Express, React.js, Node.js, Google maps Javascript and Geocode APIs, Passport authentication, Bootstrap", //
+    repo: "https://github.com/kylegold/all-speak",
+  },
+];
+
+export default projectData;
