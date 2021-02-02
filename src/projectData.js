f30b2750a00fce3ecd0e9f725d9a1a4a04d2d518
Mike <mike@mikevitelli.com>
2021-02-02 13:29:05 -0500

    changed links

--- src/projectData.js
+++ src/projectData.js
@@ -4,8 +4,7 @@ const projectData = [
     img:
       "https://github.com/kylegold/all-speak/raw/main/client/src/assets/logo/png/all_speak_v2_Logo_White.png",
     altImg: "allSpeak Logo",
-    deployedUrl:
-      "https://github.com/kylegold/all-speak/deployments/activity_log?environment=immense-journey-22247",
+    deployedUrl: "https://immense-journey-22247.herokuapp.com/",
     description:
       "allSpeak is a universal language chat room application. In these modern times, connecting with people from around the globe through the Internet has never been easier; however, there is still a significant language barrier involved. At allSpeak, we wanted to create a chatroom where people speaking multiple different languages can still connect and share experiances in the same conversation. Using allSpeak, any user can can enter a chatroom using only their own common tounge. Simply set you prefered language and allSpeak will automagically live translate all of your messages.",
     stack:
